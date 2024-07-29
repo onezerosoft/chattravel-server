056abbbc08c690db8c7d4a86e7058822bcd6c3fe
yunju <juile0827@naver.com>
2024-07-29 15:04:15 +0900

    initial commit

---
+++ chattravel/src/main/java/onezerosoft/chattravel/ChattravelApplication.java
@@ -0,0 +1,13 @@
+package onezerosoft.chattravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChattravelApplication.class, args);
+	}
+
+}
