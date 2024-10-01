b46ae95adc89d04950191c5c6d789fed1e6fb8c7
yunju <juile0827@naver.com>
2024-10-01 22:40:06 +0900

    WebConfig 수정

--- chattravel/src/main/java/onezerosoft/chattravel/config/WebConfig.java
+++ chattravel/src/main/java/onezerosoft/chattravel/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://www.chattravel.pro")
+                //.allowedOrigins("http://localhost:5173", "https://www.chattravel.pro")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With", "Accept")
                 .exposedHeaders("Authorization", "Content-Disposition")
