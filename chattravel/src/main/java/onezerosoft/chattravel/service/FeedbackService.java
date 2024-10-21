7380a54d0574573fc22dd62d52706395ab02035d
yunju <juile0827@naver.com>
2024-10-21 20:19:40 +0900

    feat<FeedbackService>: 현재 스코어 조회 API 구현 #2

--- chattravel/src/main/java/onezerosoft/chattravel/service/FeedbackService.java
+++ chattravel/src/main/java/onezerosoft/chattravel/service/FeedbackService.java
@@ -3,18 +3,21 @@ package onezerosoft.chattravel.service;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import onezerosoft.chattravel.domain.CurrentScore;
+import onezerosoft.chattravel.repository.CurrentScoreRepository;
+import onezerosoft.chattravel.repository.UserReactionRecordRepository;
 import onezerosoft.chattravel.web.dto.react.CurrentScoreResponse;
 import onezerosoft.chattravel.web.dto.react.UserReactionRequest;
 import onezerosoft.chattravel.web.dto.react.UserReactionResponse;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 
 @Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class FeedbackService {
+    private final CurrentScoreRepository currentScoreRepository;
 
     public UserReactionResponse saveUserReaction(Integer messageId, UserReactionRequest request){
         return UserReactionResponse.builder()
@@ -24,9 +27,10 @@ public class FeedbackService {
     }
 
     public CurrentScoreResponse getCurrentScore(){
+        CurrentScore currentScore = currentScoreRepository.findLatestCreated();
         return CurrentScoreResponse.builder()
-                //.currentScore(80)
-                //.createdAt(LocalDateTime.now())
+                .currentScore(currentScore.getAccuracy())
+                .createdAt(currentScore.getCreatedAt())
                 .build();
     }
 }
